105e43fa9d70b7b5fef19da1650ae3414a6cde2e
Roman <romchikus1987@gmail.com>
2023-01-09 20:27:56 +0200

    update

--- index.js
+++ index.js
@@ -52,31 +52,26 @@ const generateId = () => {
 };
 
 app.post('/api/notes', (request, response) => {
-  console.log(request.headers);
+  //console.log(request.headers);
   const body = request.body;
   if (!body.content) {
     return response.status(404).json({
       error: 'content missing',
     });
   }
-  const note = {
-    id: generateId(),
+  const note = new Note({
+    //id: generateId(),
     content: body.content,
     important: body.important || false,
     date: new Date(),
-  };
-  notes = notes.concat(note);
-  response.json(note);
-});
-
-app.get('/api/notes/', (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes);
+  });
+  note.save().then((savedNote) => {
+    response.json(savedNote);
   });
 });
 
-app.get('/api/notes/:id', (request, response) => {
-  let id = Number(request.params.id);
+app.get('/api/notes/', (request, response) => {
+  /* let id = Number(request.params.id);
   console.log(id);
   const note = notes.find((note) => note.id === id);
   console.log(note);
@@ -84,7 +79,16 @@ app.get('/api/notes/:id', (request, response) => {
     response.json(note);
   } else {
     response.status(404).end();
-  }
+  } */
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
+
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id).then((note) => {
+    response.json(note);
+  });
 });
 app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id);
