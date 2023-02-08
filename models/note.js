f28965808a500d2f596eb52f904f3e3087e56135
Roman <romchikus1987@gmail.com>
2023-02-08 15:04:16 +0200

    updates

--- models/note.js
+++ models/note.js
@@ -1,24 +1,16 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
 const mongoose = require('mongoose')
-mongoose.set('strictQuery', false)
-const url = process.env.MONGODB_URI
-console.log('connecting to', url)
-
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 const noteSchema = new mongoose.Schema({
-  content: { type: String, minLength: 5, required: true },
-  date: Date,
+  content: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
   important: Boolean,
-  checked: Boolean,
 })
 
 noteSchema.set('toJSON', {
